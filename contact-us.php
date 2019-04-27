76115cfe9ca9a97f02ef10f1ebaa469ff90d0afb
Mayank_MP5 <mayank5pande@gmail.com>
2019-04-27 23:13:18 +0530

    Two CSS libraries Added

--- contact-us.php
+++ contact-us.php
@@ -1,12 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/contact-us/contact-us_desktop">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Contact us</title>
 </head>
 <body>
+ <!--- Onimage Form --->
+
+ <div class="bg-img">
+  <form action="/action_page.php" class="container">
+    <h1>Login</h1>
+
+    <label for="email"><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <button type="submit" class="btn">Login</button>
+  </form>
+</div>
     
 </body>
 </html>
+
