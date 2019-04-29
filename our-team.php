e054aa288b6505ba76b49e02ce0219f65bc7339a
Mayank_MP5 <mayank5pande@gmail.com>
2019-04-30 04:03:22 +0530

    Our Team layout added

--- our-team.php
+++ our-team.php
@@ -7,7 +7,7 @@
     <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.2.1/css/bootstrap.min.css">-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.2.1/materia/bootstrap.min.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
-    <link rel="stylesheet" href="navbar-fixed-left.min.css">
+    <link rel="stylesheet" href="css/our-team/navbar-fixed-left.min.css">
     <link rel="stylesheet" href="docs.css">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
             integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g=" crossorigin="anonymous"></script>
