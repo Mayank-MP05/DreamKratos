49aac8726557e7cddad8788aa08a60f5a500c5be
Mayank_MP5 <mayank5pande@gmail.com>
2019-04-30 03:52:23 +0530

    Larels Index page

---
+++ laurels.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About us</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.2.1/materia/bootstrap.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <link rel="stylesheet" href="lib/css_lib/hover.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.2.1/js/bootstrap.js"></script>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+
+    <div class="container">
+
+        <!----Car 1 -->
+
+        <div class="row">
+                <h2 class="comp_name">FORMULA BHARAT ELECTRIC 2019</h2>
+                
+            </div>
+    
+            <div class="row align-items-center">
+                <div class="col-sm-4 bg-dark" id="particle1">
+                    <ul class="list-group">
+                        <li class="list-group-item right-grp-item">
+                            <p>1st - Overall Ranking</p>
+                        </li>
+                        <li class="list-group-item">
+                            
+                            <p>1st - Endurance</p>
+                        </li>
+                        <li class="list-group-item">
+                            
+                            <p>1st - Autocross</p>
+                        </li>
+                        <li class="list-group-item">
+                            
+                            <p>1st - Skidpad</p>
+                        </li>
+                        <li class="list-group-item">
+                            
+                            <p>1st - Acceleration</p>
+                        </li>
+                        <li class="list-group-item">
+                            
+                            <p>1st - Energy Efficiency</p>
+                        </li>
+                        <li class="list-group-item">
+                            
+                            <p>2nd - Business Plan</p>
+                        </li>
+                        <li class="list-group-item">
+                            
+                            <p>3rd - Cost And  Manufacturing Report</p>
+                        </li>
+    
+    
+                    </ul>
+                </div>
+    
+                <div class="col-sm-8">
+                    <img class="img img-fluid" src="images/laurels/1.png" alt="">
+                </div>
+            </div>
+    
+        
+
+        <!----Car 2 -->
+
+        <div class="row">
+            <h2 class="comp_name">CONCEPT CHALLENGE 2018</h2>
+            
+        </div>
+
+        <div class="row align-items-center">
+            <div class="col-sm-8">
+                <img class="img img-fluid" src="images/laurels/1.png" alt="">
+            </div>
+            <div class="col-sm-4 bg-dark align-middle" id="particle2">
+                <ul class="list-group">
+                    <li class="list-group-item">1st - Overall</li>
+                    <li class="list-group-item">1st - Best Procurement Report</li>
+                    <li class="list-group-item">1st - Best Team Management Strategy</li>
+                    <li class="list-group-item">1st - Best Battery Design</li>
+                </ul>
+            </div>
+        </div>
+
+        <!----Car 3 -->
+
+        <div class="row">
+                <h2 class="comp_name">SAE SUPRA 2018</h2>
+                
+            </div>
+    
+            <div class="row align-items-center">
+                <div class="col-sm-4 bg-dark" id="particle7">
+                    <ul class="list-group">
+                        <li class="list-group-item">1st - Overall Ranking</li>
+                        <li class="list-group-item">1st - AutoCross</li>
+                        <li class="list-group-item">1st - SkidPad</li>
+                        <li class="list-group-item">1st - Engineering Design</li>
+                        <li class="list-group-item">1st - CAE</li>
+                        <li class="list-group-item">1st - Business Plan</li>
+                        <li class="list-group-item">1st - Engineering Excellence</li>
+                        <li class="list-group-item">1st - Endurance</li>
+                    </ul>
+                </div>
+    
+                <div class="col-sm-8">
+                    <img class="img img-fluid" src="images/laurels/1.png" alt="">
+                </div>
+            </div>
+
+            <!----Car 4 -->
+
+        <div class="row">
+            <h2 class="comp_name">FORMULA BHARAT 2018</h2>
+            
+        </div>
+
+        <div class="row align-items-center">
+            <div class="col-sm-8">
+                <img class="img img-fluid" src="images/laurels/1.png" alt="">
+            </div>
+            <div class="col-sm-4 bg-dark" id="particle3">
+                <ul class="list-group align-middle">
+                    <li class="list-group-item">6th - Overall Ranking</li>
+                    <li class="list-group-item">1st - AutoCross</li>
+                    <li class="list-group-item">2nd - SkidPad</li>
+                </ul>
+            </div>
+        </div>
+
+
+            <!----Car 5 -->
+
+
+            <div class="row">
+                    <h2 class="comp_name">SAE SUPRA 2017</h2>
+                    
+                </div>
+        
+                <div class="row align-items-center">
+                    <div class="col-sm-4 bg-dark" id="particle4">
+                        <ul class="list-group">
+                            <li class="list-group-item">1st - Overall Ranking</li>
+                            <li class="list-group-item">1st - Business Plan</li>
+                            <li class="list-group-item">2nd - SkidPad</li>
+                            <li class="list-group-item">2nd - Endurance</li>
+                            <li class="list-group-item">2nd - Acceleration</li>
+                        </ul>
+                    </div>
+        
+                    <div class="col-sm-8">
+                        <img class="img img-fluid" src="images/laurels/1.png" alt="">
+                    </div>
+                </div>
+
+            <!----Car 6 -->
+
+                <div class="row">
+                        <h2 class="comp_name">FORMULA BHARAT 2017</h2>
+                </div>
+            
+                    <div class="row align-items-center">
+                        <div class="col-sm-8">
+                            <img class="img img-fluid" src="images/laurels/1.png" alt="">
+                        </div>
+                        <div class="col-sm-4 bg-dark" id="particle5">
+                                <ul class="list-group">
+                                    <li class="list-group-item">5th - Overall Ranking</li>
+                                    <li class="list-group-item">2nd - AutoCross</li>
+                                </ul>
+                        </div>
+                    </div>
+
+            <!----Car 7 -->
+
+            <div class="row">
+                    <h2 class="comp_name">SAE SUPRA 2016</h2>
+                    
+                </div>
+        
+                <div class="row align-items-center">
+                    <div class="col-sm-4 bg-dark"  id="particle6">
+                        <ul class="list-group">
+                            <li class="list-group-item">1st - Overall Ranking</li>
+                            <li class="list-group-item">1st - SkidPad</li>
+                            <li class="list-group-item">1st - AutoCross</li>
+                            <li class="list-group-item">1st - Endurance</li>
+                            <li class="list-group-item">1st- Fuel Economy</li>
+                        </ul>
+                    </div>
+        
+                    <div class="col-sm-8">
+                        <img class="img img-fluid" src="images/laurels/1.png" alt="">
+                    </div>
+                </div>
+
+
+            <!----Car 8 -->
+                <div class="row">
+                        <h2 class="comp_name">SAE SUPRA 2015</h2>
+                        
+                    </div>
+                    <div class="row align-items-center">
+                         <div class="col-sm-8">
+                            <img class="img img-fluid" src="images/laurels/1.png" alt="">
+                        </div>
+                        <div class="col-sm-4 bg-dark" id="particle8">
+                                <ul class="list-group">
+                                    <li class="list-group-item">32st - Overall Ranking</li>
+                                </ul>
+                        </div>
+                    </div>
+    </div>
+</body>
+</html
