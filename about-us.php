33a0ce28eb090a1b32b1cbee027762d822323605
Mayank_MP5 <mayank5pande@gmail.com>
2019-04-30 03:42:51 +0530

    About us - complete

--- about-us.php
+++ about-us.php
@@ -26,3 +26,50 @@
                 <img src="images/about-us/team-kratos-racing-whole-team-p1.jpg" alt="Whole Team" class="img-fluid">
             </div>
         </div>
+               <!----Row 2 : Paragraph 2 -->
+
+               <div class="row img2">
+            <div class="col-sm-8 ">
+                <img src="images/about-us/Team-kratos-Thunderblade.jpg" alt="Whole Team" class="img-fluid">
+            </div>
+            <div class="col-sm-4 p2">
+                <p>Driven by our motto, “TOGETHER WE RISE”,  we have successfully manufactured 3 combustion Formula-3 cars in 3 years. The genesis of the adventure began when a few passionate minds from the college set out on this journey of forming a Formula Student team. Holding spanners in their hands, chains round their neck, innovation in their minds and determination in their heart, they set out on the journey to achieve the impossible.</p>
+            </div>
+        </div>
+        
+       <!----Row 3 : full Screen Image -->
+
+       <div class="row text-center img3">
+            <div class="col-sm-12 img_div">
+                    <img src="images/about-us/thunderblade-in-race.jpg" alt="Whole Team" class="img-fluid">
+            </div>
+       </div>
+
+       <!----Row 4 : Quote -->
+
+       <div class="row text-center">
+        <div class="col-sm-12 quote_bg">
+            <h3 class="quote statement">“WE DIDN’T KNOW IT WAS IMPOSSIBLE, SO WE DID IT”.</h3>
+        </div>
+       </div>
+
+       <!----Row 5 : Quote -->
+
+       <div class="row text-center img4">
+           <div class="col-sm-3 p3">
+                <p>Making the ends meet, manufactured the first vehicle in the year 2015. But that was not all that we were capable of. Brainstorming on the solutions by unified efforts, we set in again to manufacture our next vehicle for the second season as we stood up on the podium as the overall WINNERS. Hence, the legacy of PCCOE MOTORSPORTS  took its way up and there is no looking back from here.</p>
+           </div>
+           <div class="col-sm-9">
+                <img src="images/about-us/Thunderblade-Bossy-theme.jpg" alt="Whole Team" class="img-fluid">
+           </div>
+       </div>
+
+        <!----Row 6 : Quote -->
+
+        <div class="row text-center quote_bg img1">
+                <h3 class="quote statement">Keeping up with the legacy that our mentors started we are on a quest of becoming the epitome of hard work , team spirit  and FORMULA STUDENT SUCCESS.</h3>
+        </div>
+
+  
+</div></body>
+</html>
