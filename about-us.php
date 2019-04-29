c06d2ea26bee869968b9825db02039cea23279f0
Mayank_MP5 <mayank5pande@gmail.com>
2019-04-30 03:45:02 +0530

    About us - styling done

--- about-us.php
+++ about-us.php
@@ -12,7 +12,7 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
     integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.2.1/js/bootstrap.js"></script>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/about-us/about-us-style.css">
 </head>
 <body>
 <div class="container">
