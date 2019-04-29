120b0351244f27172c109064c86c902dd4d728bd
Mayank_MP5 <mayank5pande@gmail.com>
2019-04-30 04:01:33 +0530

    Our Team page Expt

---
+++ our-team.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Bootstrap Navbar Sidebar - Fixed to Left or Right</title>
+    <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.2.1/css/bootstrap.min.css">-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.2.1/materia/bootstrap.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+    <link rel="stylesheet" href="navbar-fixed-left.min.css">
+    <link rel="stylesheet" href="docs.css">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+            integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.2.1/js/bootstrap.js"></script>
+    <script src="docs.js"></script>
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-primary fixed-left">
+   
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
+                aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link">Departments</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">Our Team</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">Vehicle Dynamics</a>
+                </li>
+    
+                <li class="nav-item">
+                    <a class="nav-link">Drivetrain System</a>
+                </li>
+    
+                <li class="nav-item">
+                    <a class="nav-link">Brakes System</a>
+                </li>
+    
+                <li class="nav-item">
+                    <a class="nav-link">Chassis System</a>
+                </li>
+    
+                <li class="nav-item">
+                    <a class="nav-link">Marketing Team</a>
+                </li>
+    
+                <li class="nav-item">
+                    <a class="nav-link">Composites System</a>
+                </li>
+    
+                <li class="nav-item">
+                    <a class="nav-link">Tractive System and<br>Accumulator</a>
+                </li>
+    
+                <li class="nav-item">
+                    <a class="nav-link">Ground Low Voltage System<br>and Data Aquisition System</a>
+                </li>
+    
+                <li class="nav-item">
+                    <a class="nav-link">Alumnis</a>
+                </li>
+        </div>
+    </nav>
+
+    <div class="container cont3">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae cum placeat nostrum voluptates consectetur aliquam pariatur, temporibus optio quam quibusdam eum, explicabo et consequatur. Repellendus aut nulla ipsa asperiores architecto voluptatem sint labore beatae officia eligendi! Hic, sed quam! Sequi earum enim harum, eligendi iste eaque expedita aut saepe porro accusantium? Culpa doloremque officia provident sequi consequuntur, minima inventore, eligendi et molestias, id numquam nulla tempora officiis necessitatibus saepe expedita velit at optio asperiores adipisci molestiae recusandae! Dicta voluptatem nihil deserunt reprehenderit nulla, natus quidem commodi aliquam qui nam veniam.</p>
+    </div>
+
+</body>
+</html>
