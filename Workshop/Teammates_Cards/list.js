93e757b829c3974eee418707f7f8aded56842d78
Mayank_MP5 <mayank5pande@gmail.com>
2019-05-05 23:01:35 +0530

    Data tabulation Done

--- Workshop/Teammates_Cards/list.js
+++ Workshop/Teammates_Cards/list.js
@@ -4,6 +4,7 @@ window.onload = function(){
 var pageCounter = 1;
 var animalContainer = document.getElementById("animal-info");
 var btn = document.getElementById("btn");
+var op = document.getElementById("op");
 
 btn.addEventListener("click", function() {
   var ourRequest = new XMLHttpRequest();
@@ -12,6 +13,7 @@ btn.addEventListener("click", function() {
     if (ourRequest.status >= 200 && ourRequest.status < 400) {
       var ourData = JSON.parse(ourRequest.responseText);
       console.log(ourData);
+      tabulate(ourData);
     } else {
       console.log("We connected to the server, but it returned an error.");
     }
@@ -29,6 +31,7 @@ btn.addEventListener("click", function() {
   }
 });
 
+/*
 function renderHTML(data) {
   var htmlString = "";
 
@@ -58,5 +61,42 @@ function renderHTML(data) {
   }
 
   animalContainer.insertAdjacentHTML('beforeend', htmlString);
+}
+*/
+
+function tabulate(data){
+
+    
+    /* default Code */
+    code1  = `<table>
+    <tr>
+        <th>Serial No.</th>
+        <th>Dept. No</th>
+        <th>Dept</th>
+        <th>Teammate Name</th>
+    </tr>`;
+
+    code2 = "";
+    count = 0;
+    code3 = "</table>";
+    for(var i=0; i < data.length ; i++){
+        console.log(data[i].name);
+
+            for(var j = 0; j < data[i].grpMember.length;j++){
+                count++;
+                
+                code2 += `
+                <tr>
+                    <td>` + count + `</td>
+                    <td>` + i + `</td>
+                    <td>` + data[i].name + `</td>`;
+
+                code2 += `<td>` + data[i].grpMember[j] + `</td>
+                </tr>`;
+
+            }
+    }
+    html = code1 + code2 + code3;
+    op.innerHTML = html;
 }
 }
