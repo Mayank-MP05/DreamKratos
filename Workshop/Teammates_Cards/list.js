a9d47c73eb46b492639b513fcdf8751b13a4f07e
Mayank_MP5 <mayank5pande@gmail.com>
2019-05-05 23:06:37 +0530

    Data Cardification Started

--- Workshop/Teammates_Cards/list.js
+++ Workshop/Teammates_Cards/list.js
@@ -13,7 +13,7 @@ btn.addEventListener("click", function() {
     if (ourRequest.status >= 200 && ourRequest.status < 400) {
       var ourData = JSON.parse(ourRequest.responseText);
       console.log(ourData);
-      tabulate(ourData);
+      cardify(ourData);
     } else {
       console.log("We connected to the server, but it returned an error.");
     }
@@ -25,10 +25,7 @@ btn.addEventListener("click", function() {
   };
 
   ourRequest.send();
-  pageCounter++;
-  if (pageCounter > 3) {
-    btn.classList.add("hide-me");
-  }
+ 
 });
 
 /*
@@ -99,4 +96,58 @@ function tabulate(data){
     html = code1 + code2 + code3;
     op.innerHTML = html;
 }
+
+function cardify(data){
+    console.log(data);
+
+    team_member = `<!-- Team member -->
+    <div class="col-xs-12 col-sm-6 col-md-4">
+        <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
+            <div class="mainflip">
+                <div class="frontside">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <p><img class=" img-fluid" src="https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_01.png" alt="card image"></p>
+                            <h4 class="card-title">Sunlimetech</h4>
+                            <p class="card-text">This is basic card with image on top, title, description and button.</p>
+                            <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="backside">
+                    <div class="card">
+                        <div class="card-body text-center mt-4">
+                            <h4 class="card-title">Sunlimetech</h4>
+                            <p class="card-text">This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.</p>
+                            <ul class="list-inline">
+                                <li class="list-inline-item">
+                                    <a class="social-icon text-xs-center" target="_blank" href="#">
+                                        <i class="fa fa-facebook"></i>
+                                    </a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a class="social-icon text-xs-center" target="_blank" href="#">
+                                        <i class="fa fa-twitter"></i>
+                                    </a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a class="social-icon text-xs-center" target="_blank" href="#">
+                                        <i class="fa fa-skype"></i>
+                                    </a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a class="social-icon text-xs-center" target="_blank" href="#">
+                                        <i class="fa fa-google"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ./Team member -->`;
+}
+
 }
