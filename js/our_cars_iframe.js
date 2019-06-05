2f26257bb1373b551c65e8f88f1acc058349309f
Mayank_MP5 <mayank5pande@gmail.com>
2019-06-05 12:22:10 +0530

    Images Added to Directory

--- js/our_cars_iframe.js
+++ js/our_cars_iframe.js
@@ -1,1 +1,27 @@
-our-cars-iframe
+
+function printName(data){
+    frame = document.getElementById("our-cars-iframe");
+
+    var data = data;
+
+    var link = ``;
+
+    switch(data){
+        case 40:link = "4.0";
+        break;
+        case 31:link = "3.1";
+        break;
+        case 30:link = "3.0";
+        break;
+        case 21:link = "2.1";
+        break;
+        case 20:link = "2.0";
+        break;
+        case 10:link = "1.0";
+        break;
+        default:link = "4.0";
+    }
+
+    var code = `ourCars/Fireblade`+link+`.html`;
+    frame.src = code;
+}
