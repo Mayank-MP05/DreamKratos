19e67133781deb20d60bd084e1275c7cc202eb49
Mayank_MP5 <mayank5pande@gmail.com>
2019-06-18 23:21:35 +0530

    HeadNav : Font Changed

--- about-us.php
+++ about-us.php
@@ -18,11 +18,10 @@
 <div class="container bigbox">
         <div class="row align-items-center img1">
             <div class="col-sm-5 p1 vcenter">
- 
-                <p>
+            <p class="normal">
                 &nbsp    &nbsp   &nbsp   &nbsp    
-                Team Kratos Racing is a Formula student team of PCCOE, comprising of 35 highly dedicated and diligent undergraduate students. Putting in hours at the workshop where sleepless nights and greasy jeans became our daily routine, we design, manufacture and test a Formula 3 styled vehicle. We promote students to hone their skills and execute all tasks to perfection. We exist to race in the top echelon of motor racing starting with national level and then taking India to the world.</p>
-            
+                Team Kratos Racing is a Formula student team of PCCOE, comprising of 35 highly dedicated and diligent undergraduate students. Putting in hours at the workshop where sleepless nights and greasy jeans became our daily routine, we design, manufacture and test a Formula 3 styled vehicle. We promote students to hone their skills and execute all tasks to perfection. We exist to race in the top echelon of motor racing starting with national level and then taking India to the world.
+            </p>
             </div>
             <div class="col-sm-7">
                 <img src="images/about-us/team-kratos-racing-whole-team-p1.jpg" alt="Whole Team" class="img-fluid">
@@ -38,7 +37,7 @@
                 <img src="images/about-us/Team-kratos-Thunderblade.jpg" alt="Whole Team" class="img-fluid">
             </div>
             <div class="col-sm-4 p2">
-                <p>
+                <p class="normal">
                         &nbsp    &nbsp   &nbsp   &nbsp 
                     Driven by our motto, “TOGETHER WE RISE”,  we have successfully manufactured 3 combustion Formula-3 cars in 3 years. The genesis of the adventure began when a few passionate minds from the college set out on this journey of forming a Formula Student team. Holding spanners in their hands, chains round their neck, innovation in their minds and determination in their heart, they set out on the journey to achieve the impossible.</p>
             </div>
@@ -57,7 +56,7 @@
 
        <div class="row text-center img align-items-center">
            <div class="col-sm-3 p3">
-                <p>Making the ends meet, manufactured the first vehicle in the year 2015. But that was not all that we were capable of. Brainstorming on the solutions by unified efforts, we set in again to manufacture our next vehicle for the second season as we stood up on the podium as the overall WINNERS. Hence, the legacy of PCCOE MOTORSPORTS  took its way up and there is no looking back from here.</p>
+                <p class="normal">Making the ends meet, manufactured the first vehicle in the year 2015. But that was not all that we were capable of. Brainstorming on the solutions by unified efforts, we set in again to manufacture our next vehicle for the second season as we stood up on the podium as the overall WINNERS. Hence, the legacy of PCCOE MOTORSPORTS  took its way up and there is no looking back from here.</p>
            </div>
            <div class="col-sm-9">
                 <img src="images/about-us/Thunderblade-Bossy-theme.jpg" alt="Whole Team" class="img-fluid">
