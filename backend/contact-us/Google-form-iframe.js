49bc1c7b9e93362925f65a194db57542d55d87e0
Mayank_MP5 <mayank5pande@gmail.com>
2019-06-25 16:36:38 +0530

    CodeCleaning Done

--- backend/contact-us/Google-form-iframe.js
+++ backend/contact-us/Google-form-iframe.js
@@ -9,15 +9,3 @@ function goto(str){
     form = document.getElementById('contact-us-form');
     form.src = src;
 }
-
-/*
-// Technical Team Link 
-
-
-<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdNk7muGRAg1_Ts9NT4nSA9J-5uE1BzYEh4PhZmomIiXqDTtA/viewform?embedded=true">Loading...</iframe>
-
-// Management Team Form
-
-<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSejIc2PW3jQiqXkWejjDNLzx1WymF_C_8-2_KmK6fJL_RV57w/viewform?embedded=true">Loading...</iframe>
-
-*/
