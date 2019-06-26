b23f577af24e59d23f67556e1316480391a64acf
Mayank_MP5 <mayank5pande@gmail.com>
2019-06-26 18:29:21 +0530

    Newswire Image Hover FOxed

--- newswire.php
+++ newswire.php
@@ -35,40 +35,38 @@
 <!-- Card Deck 1 -->
 <div class="card-deck">
 
-        <div class="card">
+        <div class="card ">
             <a href="http://www.autocarpro.in/news-national/pc-college-engineering-team-kratos-racing-wins-2016-supra-sae-india-20913">
                 <img class="card-img-top" src="images/newswire/Autocar_professional.png" alt="Autocar_professional.png">
             </a>
-            <div class="middle">
-                <div class="text">AutoCar Professional</div>
-            </div>
-            <div class="fiddle">
-                <div class="text">PC College of Engineering’s Team Kratos Racing wins 2016 SUPRA SAE India</div>
+            <div class="overlay r-google">
+                <div class="text">
+                    <h4 class="batman">AutoCar Professional</h4>
+                </div>
             </div>
         </div>
 
         
-    <div class="card">
+    <div class="card ">
         <a href="https://www.business-standard.com/article/pti-stories/saeindia-supra-draws-to-close-with-kratos-racing-winning-116070900575_1.html">
             <img class="card-img-top" src="images/newswire/business-std.jpeg" alt="business-std.jpeg">
         </a>
-        <div class="middle">
-            <div class="text">Business Standard</div>
+        <div class="overlay b-google">
+            <div class="text">
+                <h4 class="batman">Business Standard</h4>
+            </div>
         </div>
-        <div class="fiddle">
-            <div class="text">SAEINDIA SUPRA draws to close with Kratos Racing winning</div>
-        </div>
+       
     </div>
     <div class="card" >
         <a href="http://www.india.com/auto/car-news/maruti-suzuki-supra-saeindia-concludes-college-from-pune-takes-the-trophy-home-11221/">
             <img class="card-img-top" src="images/newswire/essel.jpeg" alt="essel.jpeg">
         </a>
-        <div class="middle">
-            <div class="text">India Magzine</div>
-        </div>
-        <div class="fiddle">
-            <div class="text">Maruti Suzuki SUPRA SAEINDIA concludes, college from Pune takes the trophy home</div>
-        </div>
+        <div class="overlay y-google">
+            <div class="text">
+                <h4 class="batman">India Magzine</h4>
+            </div>
+        </div>       
     </div>
 </div>
  <!-- Card Deck 2 -->
@@ -77,23 +75,21 @@
         <a href="http://www.indiancarsbikes.in/auto-news/2016-supra-saeindia-concludes-bic-152833/">
             <img class="card-img-top" src="images/newswire/indian-cars.jpeg" alt="indian-cars.jpeg">
         </a>
-        <div class="middle">
-            <div class="text">Indian Cars And Bikes</div>
-        </div>
-        <div class="fiddle">
-            <div class="text">2016 Supra SAEINDIA Concludes at BIC</div>
-        </div>
+        <div class="overlay g-google">
+            <div class="text">
+                <h4 class="batman">Indian Cars And <br>Bikes Magzine</h4>
+            </div>
+        </div>      
     </div>
 
-    <div class="card" >
+    <div class="card r-google" >
         <a href="https://www.ltd-licencetodrive.com/2016/07/team-kratos-racing-from-pune-wins-supra.html">
             <img class="card-img-top" src="images/newswire/liscence_of_drive.jpeg" alt="liscence_of_drive.jpeg">
         </a>
-        <div class="middle">
-            <div class="text">Liscence to Drive</div>
-        </div>
-        <div class="fiddle">
-            <div class="text">Team 'Kratos Racing' from Pune wins SUPRA SAEINDIA 2016</div>
+        <div class="overlay g-google">
+            <div class="text">
+                <h4 class="batman">Liscence to Drive</h4>
+            </div>
         </div>
     </div>
 
@@ -101,12 +97,10 @@
         <a href="http://www.motownindia.com/Bureau/MotorSport/439/Kratos-Racing-teamof-Pune-PC-College-of-Engineeringwins-Supra-race-Motown-IndiaBureau#.V4YiDNJ96M9">
             <img class="card-img-top" src="images/newswire/motown.jpeg" alt="motown.jpeg">
         </a>
-        <div class="middle">
-            <div class="text">Motown</div>
-        </div>
-        <div class="fiddle">
-            <div class="text">  
-                KRATOS RACING TEAM OF PUNE PC COLLEGE OF ENGINEERING WINS SUPRA RACE</div>
+        <div class="overlay b-google">
+            <div class="text">
+                <h4 class="batman">Motown Blog</h4>
+            </div>
         </div>
     </div>
 </div>
@@ -116,37 +110,37 @@
         <a href="https://punemirror.indiatimes.com/others/sunday-read/podium-finish/articleshow/60135468.cms">
             <img class="card-img-top" src="images/newswire/podium-finish.jpeg" alt="podium-finish.jpeg">
         </a>
-        <div class="middle">
-            <div class="text">Pune Mirror - IndiaTimes</div>
-        </div>
-        <div class="fiddle">
-            <div class="text">Two student teams from the city came up top at a national level racing-car</div>
+        <div class="overlay r-google">
+            <div class="text">
+                <h4 class="batman">Pune Mirror</h4>
+            </div>
         </div>
     </div>
     <div class="card">
         <a href="http://www.uniindia.com/-kratos-racing-emerge-winner-of-supra-saeindia-contest/sports/news/549154.html">
             <img class="card-img-top" src="images/newswire/united-news.jpeg" alt="united-news.jpeg">
         </a>
-        <div class="middle">
-            <div class="text">United News Of India</div>
-        </div>
-        <div class="fiddle">
-            <div class="text">'Kratos Racing' emerge winner of SUPRA SAEINDIA contest</div>
+        <div class="overlay y-google">
+            <div class="text">
+                <h4 class="batman">UniIndia News</h4>
+            </div>
         </div>
     </div>
     <div class="card">
         <a href="https://news.webindia123.com/news/Articles/India/20160710/2896782.html">
             <img class="card-img-top" src="images/newswire/webinidia.jpeg" alt="webinidia.jpeg">
         </a>
-        <div class="middle">
-                <div class="text">Web India 123</div>
+        <div class="overlay g-google">
+            <div class="text">
+                <h4 class="batman">WebIndia123 Blog</h4>
             </div>
-            <div class="fiddle">
-                <div class="text">'Kratos Racing' emerge winner of SUPRA SAEINDIA contest</div>
-            </div>
+        </div>
         </div>
 </div>
 </div>
+<style>
+
+</style>
 
 <?php include("footer.php"); ?>
 </body>
